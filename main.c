9f551ab0ba0383cd25e402cfb0eb7616b70a6e57
Jeremy Price <donomii@gmail.com>
2020-05-10 23:31:57 +0200

    Add TEST flag, improve demo output

--- main.c
+++ main.c
@@ -163,16 +163,16 @@ void ls_test() {
   const char  hello_world[]  = "Hello World.   ";
   const char  greet[] =        "How are you today? ";
 
- 
-  //Serial.printf("Adding first string of length %d, starting at %p\n", strl, hello_world);
+  //Allocate list element, we write into the empty space 
   char * target = (char *)ls_cons_blank(strlen(greet)+1, ls);
   strcpy(target, greet);
-  int strl = strlen(hello_world) + 1;
-  target = (char *)ls_cons_blank(strl, ls);
-  strcpy(target, hello_world);
+
+  //Allocate new list element, and copy the source data into the list_stack
+  ls_cons(hello_world, strlen(hello_world) + 1, ls);
 
   for (ptr i = ls_start(ls); !ls_is_end(i); i=ls_tail(i)){
-    warn("%s",(char *)ls_head(i));
+    //ls_head returns a pointer to the data for element i
+    warn("%s", (char *)ls_head(i));
   }
   warn("\n");
 }
@@ -197,8 +197,8 @@ void loop() {
 
 }
 
-/*
+#ifdef TEST
 int main() {
 	ls_test();
 }
-*/
+#endif
